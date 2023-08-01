1ed454021569f965a546819872771be77eee73ab
Abhishekp02 <126911039+Abhishekp02@users.noreply.github.com>
2023-08-01 19:23:19 +0000

    conversion & operatiobn

---
+++ conversion.js
@@ -0,0 +1,18 @@
+let score= "33"
+let result= Number(score)
+console.log(result);
+console.log(typeof result);
+let string= "33abc"
+let isNumber= Number(string)
+console.log(isNumber);
+console.log(typeof isNumber);
+let name= "Abhishek"
+let isBoolean= Boolean(name)
+console.log(isBoolean);
+let anotherName= " "
+let boolean= Boolean(anotherName)
+console.log(typeof boolean);
+console.log(boolean);
+let theNumber= 0
+let booleanValue= Boolean(theNumber)
+console.log(booleanValue);
